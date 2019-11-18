675a4cef3dd09ac8fcf878d042094d9ad61c683b
Berthier <esteban.berthier@epitech.eu>
2019-11-18 16:42:16 +0100

    add users and link to authent system spring boot

--- apps/products/src/main/java/com/cashmanager/products/configuration/SecurityConfiguration.java
+++ apps/products/src/main/java/com/cashmanager/products/configuration/SecurityConfiguration.java
@@ -4,6 +4,7 @@ package com.cashmanager.products.configuration;
 import com.cashmanager.products.security.JwtAuthenticationFilter;
 import com.cashmanager.products.security.JwtAuthorizationFilter;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -15,18 +16,33 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import javax.sql.DataSource;
 
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(securedEnabled = true)
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    DataSource dataSource;
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.cors().and()
             .csrf().disable()
             .authorizeRequests()
-            .antMatchers("/Products/**").permitAll()
-            // .antMatchers("/Carts/**").hasRole("ROLE_ADMIN")
+            .antMatchers(HttpMethod.GET, "/Products").authenticated()  // Get All
+            .antMatchers(HttpMethod.GET, "/Products/{productId:[\\d+]}").authenticated() // Get by id
+            .antMatchers(HttpMethod.POST, "/Products").hasRole("ADMIN") // Create new product
+            .antMatchers(HttpMethod.PUT, "/Products/{productId:[\\d+]}").hasRole("ADMIN") // Update product
+            .antMatchers(HttpMethod.DELETE, "/Products/{productId:[\\d+]}").hasRole("ADMIN") // Delte product
+
+            .antMatchers(HttpMethod.GET, "/Carts").authenticated() 
+            .antMatchers(HttpMethod.GET, "/Carts/{productId:[\\d+]}").authenticated() 
+            .antMatchers(HttpMethod.POST, "/Carts").authenticated() 
+            .antMatchers(HttpMethod.PUT, "/Carts/{productId:[\\d+]}").authenticated() 
+            .antMatchers(HttpMethod.DELETE, "/Carts/{productId:[\\d+]}").authenticated()
             .anyRequest().authenticated()
             .and()
             .addFilter(new JwtAuthenticationFilter(authenticationManager()))
@@ -35,16 +51,11 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
             .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
     }
 
-    @Override
-    public void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.inMemoryAuthentication()
-            .withUser("user")
-            .password(passwordEncoder().encode("password"))
-            .authorities("ROLE_USER");
-        // auth.inMemoryAuthentication()
-        //     .withUser("user-admin")
-        //     .password(passwordEncoder().encode("password-admin"))
-        //     .authorities("ROLE_ADMIN");
+    @Autowired
+    public void configAuthentication(AuthenticationManagerBuilder auth) throws Exception {
+      auth.jdbcAuthentication().dataSource(dataSource)
+          .usersByUsernameQuery("select username,password, enabled from users where username=?")
+          .authoritiesByUsernameQuery("select username, role from user_roles where username=?");
     }
 
     @Bean
